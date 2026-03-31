b82944e4023fc0d15e08e4e1817549d95643c3fd
wuk26macewan <wuk26@mymacewan.ca>
2026-03-31 23:38:20 +0000

    shuffled things around to more accurate locations to compile

--- DB.h
+++ DB.h
@@ -20,6 +20,45 @@
  * WHICH WILL BE FIELDS IN THE STRUCT DataBase BELOW. YOU MAY DECLARE ADDITIONAL
  * STRUCTS AS NEEDED.
  */
+typedef struct It{
+    int ID;
+    char *tabletype;
+    char *material;
+    char *structural;
+    char *street;
+    int neighbourhoodID;
+    char *neighbourhoodName;
+    char *ward;
+    float latitude;
+    float longitude;
+    char *location;
+
+    
+    struct individual_table *tableType_next;
+    struct individual_table *surfaceMaterial_next;
+    struct individual_table *structuralMaterial_next;
+    struct individual_table *neighborhood_next;
+    struct individual_table *picnicTable_next;
+} individual_table;
+
+typedef struct table{
+    int numElems;
+    int capacity;
+    individual_table ** arr;
+} Table;
+
+typedef struct Nt{
+    int numElems;
+    int capacity;
+    individual_table ** arr;
+} NeighbourhoodTable;
+
+typedef struct Pt{
+    int numElems;
+    int capacity;
+    individual_table ** arr;
+} PicnicTable;
+
 
 /*
  * The INIT_SIZE is only relevant if you are using arrays for your data structures. 
